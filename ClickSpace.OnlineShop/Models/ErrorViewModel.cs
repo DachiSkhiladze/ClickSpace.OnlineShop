2ed84ba0acfbf8292580a71760fc4337ce56b1e1
Dachi <Dachi@DESKTOP-5K1Q4JV>
2022-05-23 12:44:19 +0400

    Deleted extra files

--- ClickSpace.OnlineShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClickSpace.OnlineShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
